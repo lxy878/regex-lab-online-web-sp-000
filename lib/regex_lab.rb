748deb1dfbd88acb59bc5265aa660eb6d5b26ec2
Xiaoyi Liu <lxy878@gmail.com>
2020-03-03 11:57:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  puts word.scan(/[AEIOUaeiou].*/)
+  return word.scan(/[AEIOUaeiou].*/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -17,3 +17,5 @@ end
 def valid_phone_number?(phone)
 
 end
+
+puts starts_with_a_vowel("Excellent")
