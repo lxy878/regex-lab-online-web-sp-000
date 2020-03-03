a46dbe9d2dce3d8375679dea99f644def9f13691
Xiaoyi Liu <lxy878@gmail.com>
2020-03-03 12:31:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-   return word.match(/^[aeiouAEIOU]\w*/) {|match| match,nil?}
+   return word.match(/^[aeiouAEIOU]\w*/) {|match| !match.empty?}
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
