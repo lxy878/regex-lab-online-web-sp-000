00ab37c8f9b4fa1bf3ae855a543fe6fbbda54841
Xiaoyi Liu <lxy878@gmail.com>
2020-03-03 13:53:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  return !phone.grep(/(\(|)\d{3}(\)|\s|)\d{3}(-|\s|)\d{4}$/)
+  return phone.grep(/(\(|)\d{3}(\)|\s|)\d{3}(-|\s|)\d{4}$/)
 end
 
 s  = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
