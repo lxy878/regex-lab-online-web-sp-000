adbc5fe494200d1470afbcd076def8781f2ffd07
Xiaoyi Liu <lxy878@gmail.com>
2020-03-03 13:30:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  return text.match?(//)
 end
 
 s  = "I wondered why the baseball was getting bigger. Then it hit me."
